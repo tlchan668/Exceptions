df22875975a6d9c645a74b704eac1c117685b640
Traci Chan <tlc668@gmail.com>
2020-02-19 18:59:59 -0500

    Add project files.

---
+++ Exceptions/BootcampException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions {
+    public class BootcampException : Exception {
+
+        public BootcampException() : base() { }
+
+        public BootcampException(string message) : base(message) { }
+
+        public BootcampException(string message, Exception InnerException): base(message, InnerException) { }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exceptions {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                var a = 1;
+                var b = 1;
+                try {
+                    var x = "ABC";
+                    x = null;
+                    var len = x.Length;
+                } catch (NullReferenceException ex) {
+                    //create our own bootcamp expection 
+                    var bce = new BootcampException("Boot camp exception", ex);//create an instance of our bootcamp exc
+                    throw bce;
+
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                }
+
+                try {
+                    var c = a / (b - b);
+
+                } catch (DivideByZeroException ex) {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }catch (Exception ex) {
+                //do something
+            }
+
+        }
+    }
+}
